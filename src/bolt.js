06d7244d36d7faf9b4a842faf2c8a79b3cc7d163
Justin Bankes <jbankes1@gmail.com>
2020-02-09 11:12:09 -0700

    added block to barbara for 'about'

--- src/bolt.js
+++ src/bolt.js
@@ -32,9 +32,30 @@ app.action('button_click', ({ body, ack, say }) => {
   say(`<@${body.user.id}> clicked the button`);
 });
 
+app.message(/about/, ({ message, say }) => {
+  say({
+    blocks: [{
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "Hello! :waving: I'm Barbara and I will help set up interviews for you!"
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "To get started run `@barbara create interview for <candidate name>`"
+        }
+      }
+    ]
+  });
+});
+
 app.message(/^create interview for [a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)+/, ({ context }) => {
   const applicantName = context.matches[0].replace('create interview for ', '');
   console.log(applicantName);
 });
+
 
 module.exports = app;
